67dd21e121cfcf024d617cdc05e064388c0fa171
Luu Truong <truonglv@outlook.com>
2019-01-04 16:41:08 +0700

    Removed unused extend

--- _output/extension_hint.php
+++ _output/extension_hint.php
@@ -2,8 +2,3 @@
 
 // ################## THIS IS A GENERATED FILE ##################
 // DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
-
-namespace Truonglv\ContentAnalytics\XF\Pub\Controller
-{
-	class XFCP_Forum extends \XF\Pub\Controller\Forum {}
-}
